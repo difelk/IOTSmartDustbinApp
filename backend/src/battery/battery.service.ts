a2bdbb39392014ac8219196ea1554b99ad622e4e
difelk <dilshan.f@theavo.net>
2023-11-13 19:20:27 +0530

    Add NestJS backend

---
+++ backend/src/battery/battery.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { BatteryData } from './battery.types';
+
+@Injectable()
+export class BatteryService {
+  getBatteryData(): BatteryData[] {
+    return [
+      { date: '2023-11-09', time: '18:13', percentage: 25 },
+      { date: '2023-11-09', time: '18:14', percentage: 30 },
+      { date: '2023-11-10', time: '08:15', percentage: 20 },
+    ];
+  }
+}
