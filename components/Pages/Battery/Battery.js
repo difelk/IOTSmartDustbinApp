136ddd6aa7364f2253227ad02c750fbe69003983
difelk <dilshan.f@theavo.net>
2023-11-14 22:07:48 +0530

    battery page ui changes V2

--- components/Pages/Battery/Battery.js
+++ components/Pages/Battery/Battery.js
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from "react";
 import { View, Text } from "react-native";
 import BatteryStyle from "./BatteryStyle";
 import Footer from "../../Footer/Footer";
+import IconButton from "../../share/Button/IconButton";
 
 export default function Battery() {
+  const refreshIcon = require("../../../assets/refresh.png")
   const [batteryDetails, setBatteryDetails] = useState([]);
   const [error, setError] = useState(null);
 
@@ -13,7 +15,7 @@ export default function Battery() {
     fetch(`http://${ipAddress}:3000/battery`)
       .then((response) => {
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         return response.json();
       })
@@ -31,13 +33,40 @@ export default function Battery() {
 
   console.log("batteryDetails - ", batteryDetails);
 
+
+const handleRefresh = () => {}
+
   return (
     <>
+    
       <View style={BatteryStyle.mainWrapper}>
+      <Text style={BatteryStyle.pageName}>BATTERY</Text>
         {error ? (
           <Text>Error fetching data. Please try again.</Text>
         ) : (
           <View>
+            <View style={BatteryStyle.currentStatus}>
+              <Text style={BatteryStyle.currentStatusTitle}>Current Battery Status</Text>
+              <View style={BatteryStyle.currentStatusDisplay}>
+                <Text style={BatteryStyle.lardgeTxt}>80</Text>
+                <Text  style={BatteryStyle.perc}>%</Text>
+              </View>
+              <View style={BatteryStyle.refreshBtn}>
+              <IconButton
+                key={1}
+                onPress={() => handleRefresh()}
+                buttonText={""}
+                buttonIcon={refreshIcon}
+                bgColor={"#FFFFFF"}
+                txtColor={"#FFFFFF"}
+                width={25}
+                height={25}
+                btnSize={"SM"}
+                btnType={""}
+              />
+              </View>
+            </View>
+
             <View style={BatteryStyle.headerTxt}>
               <Text style={BatteryStyle.mainTitleTxt}>Battery History</Text>
             </View>
@@ -54,22 +83,23 @@ export default function Battery() {
               </View>
             </View>
 
-            {batteryDetails && batteryDetails.map((data, index) => (
-              <View
-                style={BatteryStyle.dataBody}
-                key={`${data.date}-${data.time}-${index}`}
-              >
-                <View style={BatteryStyle.dataBodyCol}>
-                  <Text>{data.date}</Text>
+            {batteryDetails &&
+              batteryDetails.map((data, index) => (
+                <View
+                  style={BatteryStyle.dataBody}
+                  key={`${data.date}-${data.time}-${index}`}
+                >
+                  <View style={BatteryStyle.dataBodyCol}>
+                    <Text>{data.date}</Text>
+                  </View>
+                  <View style={BatteryStyle.dataBodyCol}>
+                    <Text>{data.time}</Text>
+                  </View>
+                  <View style={BatteryStyle.dataBodyCol}>
+                    <Text>{data.percentage}%</Text>
+                  </View>
                 </View>
-                <View style={BatteryStyle.dataBodyCol}>
-                  <Text>{data.time}</Text>
-                </View>
-                <View style={BatteryStyle.dataBodyCol}>
-                  <Text>{data.percentage}%</Text>
-                </View>
-              </View>
-            ))}
+              ))}
           </View>
         )}
       </View>
--- components/Pages/Battery/BatteryStyle.js
+++ components/Pages/Battery/BatteryStyle.js
@@ -3,6 +3,7 @@ import { Colors } from "react-native/Libraries/NewAppScreen";
 import { colors } from "../../../styles/constants";
 
 export default StyleSheet.create({
+  
   mainWrapper: {
     width: "100%",
     display: "flex",
@@ -13,12 +14,75 @@ export default StyleSheet.create({
     paddingRight: 8,
   },
 
+  pageName:{
+    display:'flex',
+    alignItems: 'flex-start',
+    color: '#b97ae5',
+    fontSize: 16,
+    fontWeight: '500',
+    marginBottom: 20,
+
+  },
+
+  currentStatusTitle: {
+    color: "#ffffff",
+    marginTop: 20,
+  },
+
+  currentStatus: {
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#4e6bf2",
+    padding: 8,
+    borderRadius: 4,
+    marginBottom: 15,
+    ...Platform.select({
+      ios: {
+        shadowColor: "blue",
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.1,
+        shadowRadius: 20,
+      },
+      android: {
+        shadowColor: "black",
+        elevation: 20,
+      },
+    }),
+  },
+
+  currentStatusDisplay: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
   mainTitleTxt: {
     fontSize: 15,
     color: colors.header2,
     fontWeight: "600",
     marginTop: 8,
     marginBottom: 8,
+  },
+
+  lardgeTxt: {
+    fontSize: 70,
+    fontWeight: "700",
+    color: "#ffffff",
+  },
+
+  perc: {
+    fontSize: 15,
+    color: "#ffffff",
+  },
+
+  refreshBtn: {
+    display: "flex",
+    width: "100%",
+    alignItems: "flex-end",
+    marginRight: 18,
   },
 
   headerSection: {
