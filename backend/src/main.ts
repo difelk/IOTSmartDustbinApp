512978dc764476a716817afa93e80e69f12611ed
difelk <dilshan.f@theavo.net>
2023-11-14 18:12:39 +0530

    database configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,21 @@
-// src/main.ts
+// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as admin from 'firebase-admin';
+import { firebaseConfig } from '../config/firebaseConfig';
 
 async function bootstrap() {
+  const serviceAccount = firebaseConfig as admin.ServiceAccount;
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL:
+      'https://nest-native-iot-smart-dustbin-default-rtdb.firebaseio.com/',
+  });
+
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
   await app.listen(3000);
 }
+
 bootstrap();
